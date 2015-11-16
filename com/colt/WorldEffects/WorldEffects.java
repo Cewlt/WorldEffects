c630da3975fb7b98419118d27988ca2a39b3be87
Colt <Cewlt@users.noreply.github.com>
2015-11-16 14:29:39 -0500

    this was only like a year behind.

--- com/colt/WorldEffects/WorldEffects.java
+++ com/colt/WorldEffects/WorldEffects.java
@@ -3,32 +3,69 @@ package com.colt.WorldEffects;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class WorldEffects extends JavaPlugin implements Listener {
 	
+	PotionEffect effect;
+	
+	List<String> worlds = getConfig().getStringList("worlds");
+	List<String> effects = getConfig().getStringList("effects");
+	
+	World world;
+	int dur;
+	int amp;
+	
 	public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
 		saveDefaultConfig();
-        	Bukkit.getPluginManager().registerEvents(this, this);
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                checkEffects();
+            }
+        }, 100L, 60L);
 	}
 	
-	@EventHandler
-	public void move(PlayerInteractEvent e) {
-		Player p = e.getPlayer();
-		List<String> worlds = getConfig().getStringList("worlds");
-		for(String w : worlds) {
-			if(p.getWorld().equals(w) &&
-				(!p.hasPotionEffect(PotionEffectType.JUMP) || (!p.hasPotionEffect(PotionEffectType.JUMP)))) {
-					PotionEffect jump = new PotionEffect(PotionEffectType.JUMP, 100000000, getConfig().getInt("jumpamp"));
-					p.addPotionEffect(jump);
-					PotionEffect speed = new PotionEffect(PotionEffectType.SPEED, 100000000, getConfig().getInt("speedamp"));
-					p.addPotionEffect(speed);
+	public void checkEffects() {
+		for (Player p : getServer().getOnlinePlayers()) {
+			for(String w : worlds) {
+				if(Bukkit.getWorld(w) != null) {
+					world = Bukkit.getWorld(w);
+					if(p.getWorld().equals(world)) {
+						for(String po : effects) {
+							String inf[] = po.split(":");
+							if(inf.length == 3) {
+								if(PotionEffectType.getByName(inf[0].toUpperCase()) != null) {
+									PotionEffectType pot = PotionEffectType.getByName(inf[0].toUpperCase());
+									try {
+										dur = Math.min(Integer.parseInt(inf[1]), Integer.MAX_VALUE);
+									} catch(NumberFormatException e1) {
+										dur = Integer.MAX_VALUE;
+									} 
+									try {
+										amp = Math.min(Integer.parseInt(inf[2]), 255);
+									} catch(NumberFormatException e1) {
+										amp = 255;
+									}
+									if(!p.hasPotionEffect(pot)) {
+										effect = new PotionEffect(pot, dur, amp);
+										p.addPotionEffect(effect);	
+									} else {
+										p.removePotionEffect(pot);
+										effect = new PotionEffect(pot, dur, amp);
+										p.addPotionEffect(effect);
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
